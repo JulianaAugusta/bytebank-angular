723df973193e55b3fd8efec36754c6753844595b
Camilla Falcão <millafalcao.acad@gmail.com>
2025-05-26 01:13:35 -0300

    refactor: reorganizing routes and implementation of auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,26 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from '@core/guards/auth.guard';
+import { BaseComponent } from '@pages/base/base.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { 
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: '',
+    component: BaseComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+      },
+    ]
+  }
 ];
