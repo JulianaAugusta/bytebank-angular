57ad50452882d6f2c34594e3b0bc4843051a8c92
JuliAugusta <juli_aug@hotmail.com>
2025-05-31 10:10:45 -0300

    feature: dynamically update balance

--- src/app/components/account-balance/account-balance.component.ts
+++ src/app/components/account-balance/account-balance.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import {
+  Transaction,
+  TransactionService,
+} from '@core/services/transaction.service';
 
 @Component({
   selector: 'app-account-balance',
@@ -9,13 +13,32 @@ import { MatCardModule } from '@angular/material/card';
   imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './account-balance.component.html',
   styleUrl: './account-balance.component.scss',
-  providers: [CurrencyPipe, DatePipe]
+  providers: [CurrencyPipe, DatePipe],
 })
-export class AccountBalanceComponent {
-  balance = 30000;
+export class AccountBalanceComponent implements OnInit {
+  balance = 0;
   showBalance = true;
 
-  toggleBalanceVisibility() {
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadBalance();
+
+    this.transactionService.transactionsChanged$.subscribe(() => {
+      this.loadBalance();
+    });
+  }
+
+  private loadBalance(): void {
+    this.transactionService.getTransactions().subscribe((transactions: Transaction[]) => {
+      this.balance = transactions.reduce((total, t) => {
+        const amount = Number(t.amount);
+        return t.type === 'income' ? total + amount : total - amount;
+      }, 0);
+    });
+  }
+
+  toggleBalanceVisibility(): void {
     this.showBalance = !this.showBalance;
   }
 
