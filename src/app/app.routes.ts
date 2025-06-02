720299c98cd34f42faaeea718a143c94a755f67c
Camilla Falcão <millafalcao.acad@gmail.com>
2025-06-01 21:50:33 -0300

    feat: update routing to redirect to dashboard (home)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { authGuard } from '@core/guards/auth.guard';
 import { BaseComponent } from '@pages/base/base.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+  { 
+    path: '', 
+    redirectTo: 'dashboard',
+    pathMatch: 'full' 
   },
   {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  { 
     path: 'login',
     loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
   },
@@ -18,8 +22,8 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       {
-        path: 'home',
-        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
       },
     ]
   }
