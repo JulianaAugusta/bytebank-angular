d043c6166a6942cb3042f45cd2903a67ee17805b
Camilla Falcão <millafalcao.acad@gmail.com>
2025-06-02 01:50:51 -0300

    feat: remove login route from application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,10 +12,6 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
-  { 
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
-  },
   {
     path: '',
     component: BaseComponent,
