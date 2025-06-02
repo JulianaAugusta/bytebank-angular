6cf50d68fe4d1853b3e3bcb5edb15cba019256bc
Camilla Falcão <millafalcao.acad@gmail.com>
2025-06-02 01:52:16 -0300

    feat: refactor Navbar component for improved structure and functionality

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,18 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '@pages/login/login.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatToolbarModule, 
+    RouterModule
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent implements OnInit {
+  dialog = inject(MatDialog);
   router = inject(Router);
   currentUrl = signal(this.router.url);
   isHomePage = computed(() => this.currentUrl() === '/home');
@@ -20,6 +30,14 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
     this.router.events.subscribe(() => {
       this.currentUrl.set(this.router.url);
+    });
+  }
+
+  openAccountDialog(signupMode: boolean): void {
+    const dialogRef = this.dialog.open(LoginComponent, { data: signupMode, height: '80%' });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
     });
   }
 }
