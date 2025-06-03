c343f0368b09def1be797b37bf40403f6d386816
Camilla Falcão <millafalcao.acad@gmail.com>
2025-06-02 22:35:43 -0300

    Update auth.interceptor.ts
    feat: added expired token toast

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,13 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+import { NotificationService } from '@shared/services/notification.service';
 import { EMPTY } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
+  const notificationService = inject(NotificationService);
 
   //@to do validar exibição de toast para token expirado
   if (!auth.isAuthenticated() && !req.url.includes('users')) {
+    notificationService.showToast('Sessão expirada', 'error');
     auth.logout(); 
     return EMPTY //bloqueio ou encerra a requisição sem lançar erro
   }
