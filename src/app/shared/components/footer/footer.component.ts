3486fc01c015bbb8eecf66585e568d65a45b490d
Camilla Falcão <millafalcao.acad@gmail.com>
2025-06-02 21:11:16 -0300

    feat: add responsive design for footer component and implement mobile detection

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  breakpointObserver = inject(BreakpointObserver);
+  isMobile = signal(false);
 
+  constructor() {
+    this.breakpointObserver.observe([Breakpoints.Handset, '(max-width: 720px)'])
+      .subscribe(result => {
+        this.isMobile.set(result.matches);
+      });
+  }
 }
